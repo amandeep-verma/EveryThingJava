55e56f3ba9679b4bc6f6d295055bf1ce115470d7
amandeep-verma <39786879+amandeep-verma@users.noreply.github.com>
2021-05-27 08:21:03 -0400

    StreamAPI

--- EverythingJava/src/_11_Java8/_02_main.java
+++ EverythingJava/src/_11_Java8/_02_main.java
@@ -1,36 +1,45 @@
 package _11_Java8;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Stream;
 
-//V13-V1 Telusko 
+//V13 Telusko 
 
 /*
  Stream API has lots of method to process Huge amount of data (bigData, Hadoop)
- Also we have Intermediate methods and Terminate methods.
- Once you have used Stream of data, you can't reuse it.
+ Stream operations are divided into intermediate and terminal operations. A stream pipeline is followed by zero 
+ or more intermediate operations and a terminal operation.
  
+ Intermediate operations return a new stream. They are always lazy; executing an intermediate operation such as 
+ filter() does not actually perform any filtering, but instead creates a new stream. ex- filter(), map(), mapToInt()
  
+ Terminal operations may traverse the stream to produce a result. After the terminal operation is performed, the 
+ stream pipeline is considered consumed, and can no longer be used. ex- findFirst(), forEach(), reduce()
+ exceptions- iterator() and spliterator(); these are provided as an "escape hatch".
  */
 
 public class _02_main {
 	public static void main(String[] args) {
 
 		List<Integer> values = new ArrayList<Integer>();
-		for(int i =1; i<=100;i++)
+		for(int i =1; i<=10;i++)
 		{
 			values.add(i);
 		}
 		
-		// We have use forEach method in previous class
+		// We have used forEach method in previous class
 		// The List can be converted into stream of data by
 		values.stream().forEach(System.out::println);
 		
 		System.out.println("*************");
 		// We can use parallel stream as well, which makes use of threads
 		values.parallelStream().forEach(System.out::println);
+		
+		// Using same stream again will throw error
+		Stream<Integer> s = values.stream();
+		s.forEach(System.out::println);
+//		s.forEach(System.out::println); // comment it to prevent error
 		
 	}
 }
