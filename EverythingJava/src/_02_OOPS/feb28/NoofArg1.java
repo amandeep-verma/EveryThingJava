7f2090827602a38c11d8550a3449b2164691fd01
amandeep-verma <39786879+amandeep-verma@users.noreply.github.com>
2023-10-03 11:30:03 -0400

    OOPS

--- EverythingJava/src/_02_OOPS/feb28/NoofArg1.java
+++ EverythingJava/src/_02_OOPS/feb28/NoofArg1.java
@@ -1,7 +1,15 @@
 package _02_OOPS.feb28;
 
 /* 
-OOP (Object Oriented Programming) are Polymorphism, Inheritance, Abstraction  and Encapsulation
+OOP (Object Oriented Programming) are 
+1. Polymorphism
+2. Inheritance -  child class inherits the property of parent class by extends keyword
+3. Abstraction - a process of hiding the implementation details from the user, only the functionality will be provided.
+	By using abstract method/ abstract class(interface). 
+4. Encapsulation -  wrapping the data (variables) and code acting on the data (methods) together as a single unit.
+	Achieved by using getter and setters for variables and making variables private.
+
+
 Polymorphism: is the ability of an object to take on many forms.
 There are 2 types of polymorphism which are commonly mentioned.
 1. Compile-Time Polymorphism/ Early binding/ Static binding : Method Overloading
